56a53ca7eb83184279a0a30c59e03438bfda498b
BARRY27 <barry.on@hotmail.com>
2020-12-17 11:59:40 +0100

    1er commit

---
+++ my-village/src/App.vue
@@ -0,0 +1,23 @@
+<template>
+  <div >
+  
+    <HelloWorld msg="Site de Fatiha "/>
+
+    
+  </div>
+</template>
+
+<script>
+import HelloWorld from './components/HelloWorld.vue'
+
+export default {
+  name: 'App',
+  components: {
+    HelloWorld
+  }
+}
+</script>
+
+<style>
+
+</style>
